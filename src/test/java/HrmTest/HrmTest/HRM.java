cae4d7cce3e02909d93ffd0e90b9b90226262f63
ahmad0711 <ahmadalich13@gmail.com>
2024-05-15 17:55:58 +0500

    done

--- src/test/java/HrmTest/HrmTest/HRM.java
+++ src/test/java/HrmTest/HrmTest/HRM.java
@@ -1,7 +1,6 @@
 package HrmTest.HrmTest;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -28,49 +27,76 @@ public class HRM {
 
 		driver.get(url);
 
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
 
 	}
 
 	//	Start Test Cases Here 
 
-	@Test
+	@Test (priority = 2)
 
-	public void login() {
-		
+	public void loginValid() {
+
 		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
-		
+
 		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
-		
+
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-	
-	
-	String pageTitle = driver.getTitle();
-	
-/*	if (pageTitle.equals("OrangeHRM")) {
-	
+
+
+		String pageTitle = driver.getTitle();
+
+		/*	if (pageTitle.equals("OrangeHRM")) {
+
 		System.out.println("Your test case is passed");
 	} else {
-	
+
 		System.out.println("Your test case is failed");
 	} */
-	
-//	we can also use assertions of validate test 
-	
 
-	Assert.assertEquals("OrangeHRM", pageTitle);
+		//	we can also use assertions of validate test 
+
+
+		Assert.assertEquals("OrangeHRM", pageTitle);
+
+		
+
+	}
 	
-	driver.findElement(By.xpath("//span[@class='oxd-userdropdown-tab']")).click();
-	
-	driver.findElement(By.xpath("//a[normalize-space()='Logout']")).click();
-	
+	public void logout() {
+		
+		driver.findElement(By.xpath("//span[@class='oxd-userdropdown-tab']")).click();
+
+		driver.findElement(By.xpath("//a[normalize-space()='Logout']"));
+		
+	}
+
+	@Test (priority = 1)
+
+	public void invalidLogin() {
+
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("123");
+
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+		String ErrorMessage = "Invalid credentials";
+
+		String mesageAct = driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p oxd-alert-content-text']")).getText();
+
+		Assert.assertTrue(ErrorMessage.contains(mesageAct));
 	}
 
 	@AfterTest 
 
 	public void tearDown() throws InterruptedException {
 
+
+		
+		logout();
+		
 		Thread.sleep(5000);
 		driver.close();
 		driver.quit();
